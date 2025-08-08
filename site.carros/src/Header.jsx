9162ae5606ac9a342534600a51c0846b67e49e98
dadotola <dadotola@gmail.com>
2025-08-08 15:51:07 -0300

    conclusao do footer e header

--- site.carros/src/Header.jsx
+++ site.carros/src/Header.jsx
@@ -3,24 +3,24 @@ function Header() {
     <>
       <header>
         <div className="background-top">
-        <div className="header-contact">
-          <img src="./imagens.projetos/Vector.svg" alt="telefone" />
-            <p className="contact">comunique-se conosco</p>
+          <div>
+            <img src="./imagens.projetos/Vector.svg" alt="telefone"></img>
+          </div>
+          <div className="header-contact">
+            <p className="contact">comunique-se conosco!</p>
             <p className="number">+48 501 140 391</p>
           </div>
-          <div />
+          <div>
+            <img src="./imagens.projetos/flame-svg.svg" alt="logo"></img>
+          </div>
           <div className="header-mail">
-            <img src="./imagens.projetos/flame-svg.svg" alt="logo" />
-            <div>
-            <img src="./imagens.projetos/Group 10.svg" alt="item" />
-            <p className="comunicacao">nosso e-mail abaixo</p>
+            <p className="comunicacao">nosso e-mail abaixo!</p>
             <p className="email">biuro@sexycars.pl</p>
-            </div>
-            
+          </div>
+          <div>
+            <img src="./imagens.projetos/Group 10.svg" alt="item"></img>
           </div>
         </div>
-
-        <nav></nav>
       </header>
     </>
   );
