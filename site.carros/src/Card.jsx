0a403a5cf1144683bec3c0875eded8fe5c47bf10
dadotola <dadotola@gmail.com>
2025-08-16 12:10:51 -0300

    finalizacao body

--- site.carros/src/Card.jsx
+++ site.carros/src/Card.jsx
@@ -4,13 +4,28 @@ function Card({ nome, imagem, tipo, motor, cor, potencia, preco }) {
       <card>
         <div className="card">
           <h2>{nome}</h2>
-          <img src={imagem} alt={nome}></img>
+
+          
+
           <div className="categoria">
+          <img className="car" src={imagem} alt={nome}></img>
             <ul>
-              <li><img src="imagens.projetos/Group 2.svg"></img>{tipo}</li>
-              <li><img src="imagens.projetos/Group 1.svg"></img>{motor}</li>
-              <li><img src="imagens.projetos/Group 3.svg"></img>{cor}</li>
-              <li><img src="imagens.projetos/Group 4.svg"></img>{potencia}</li>
+              <li>
+                <img src="imagens.projetos/Group 2.svg"></img>
+                {tipo}
+              </li>
+              <li>
+                <img src="imagens.projetos/Group 1.svg"></img>
+                {motor}
+              </li>
+              <li>
+                <img src="imagens.projetos/Group 3.svg"></img>
+                {cor}
+              </li>
+              <li>
+                <img src="imagens.projetos/Group 4.svg"></img>
+                {potencia}
+              </li>
             </ul>
           </div>
           <p className="preco">Wynajem Cena Brutto: {preco}zł</p>
